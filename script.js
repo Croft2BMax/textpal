fdd34d1d295ce84b1d0d33f5a7b8c812306676c5
Croft2BMax <110388781+Croft2BMax@users.noreply.github.com>
2022-10-18 17:59:31 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+let menuList = document.getElementById("menuList");
+
+	menuList.style.maxHeight = "0px";
+	
+	function togglemenu() {
+        if (menuList.style.maxHeight == "0px") {
+            menuList.style.maxHeight = "190px";
+        }
+        else {
+            menuList.style.maxHeight = "0px";
+        }
+    }
